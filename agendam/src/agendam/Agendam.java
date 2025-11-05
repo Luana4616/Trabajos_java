42e6dcad499fa02a8bc18e43307ad327d1e4dda8
Luana4616 <luanagiselfarina@gmail.com>
2025-11-05 18:10:10 -0300

    Turnero

---
+++ agendam/src/agendam/Agendam.java
@@ -0,0 +1,18 @@
+
+package agendam;
+import agendam.ibu.principal;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Agendam {
+
+     public static void main(String[] args) {
+       principal princ = new principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+    
+}
