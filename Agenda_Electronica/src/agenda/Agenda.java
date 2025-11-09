bd48364a320cb70bec5759da055ecb04a1ffd2d3
Luana4616 <luanagiselfarina@gmail.com>
2025-11-09 13:42:51 -0300

    Agenda electronica

---
+++ Agenda_Electronica/src/agenda/Agenda.java
@@ -0,0 +1,16 @@
+
+package agenda;
+import agenda.ibu.Principal;
+
+public class Agenda {
+
+    
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
